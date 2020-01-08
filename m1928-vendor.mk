c948a88af7529e978a44dd8c33ee2c334d4f1be2
XiNGRZ <chenxingyu92@gmail.com>
2020-01-08 17:21:31 +0800

    m1928: Add SDM blobs
    Change-Id: I05ae454ca46032d27ec80bba045accb5790cedd7

--- m1928-vendor.mk
+++ m1928-vendor.mk
@@ -17,3 +17,6 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/meizu/m1928
 
+PRODUCT_COPY_FILES += \
+    vendor/meizu/m1928/proprietary/lib64/vendor.display.color@1.0.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/vendor.display.color@1.0.so \
+    vendor/meizu/m1928/proprietary/product/lib64/libsdm-disp-apis.qti.so:$(TARGET_COPY_OUT_PRODUCT)/lib64/libsdm-disp-apis.qti.so
